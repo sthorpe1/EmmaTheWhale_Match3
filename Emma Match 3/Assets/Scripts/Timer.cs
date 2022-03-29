b291b44208bdbad27943d68788db50b0f55eff75
sthorpe1 <31780670+sthorpe1@users.noreply.github.com>
2022-03-29 10:52:08 -0700

    Game over screen w/ audio
    Added a game over screen that displays final score as well as two options (New Game and Back to Menu)
    
    Additionally Shore Buddies audio has been added to the background with a temporary mute button.

--- Emma Match 3/Assets/Scripts/Timer.cs
+++ Emma Match 3/Assets/Scripts/Timer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class Timer : MonoBehaviour
     public float timeRemaining = 60;
     //public float timeDisplay = timeRemaining;
     public bool timerRunning = false;
+    //Game Over and Game Active object references are created here
+    public GameObject gameOverMenu;
+    public GameObject gameActiveMenu;
+    // object reference for game scores and score manager script
+    [SerializeField] Text TextScore;
+    [SerializeField] Text FinalScore;
+    ScoreManager scoreManager;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,10 +36,19 @@ public class Timer : MonoBehaviour
             }
             else
             {
+                // opens the Game Over screen once timer runs out
                 timeRemaining = 0;
                 timerRunning = false;
+                gameOverMenu.SetActive(true);
+                FinalScore.text = "Final Score: " + TextScore.text;
             }
         }
         TimeLabelUI.text = timeRemaining.ToString("F0");
+    }
+
+    public void ResetTime()
+    {
+        // disables the Game Over screen and resets values
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
